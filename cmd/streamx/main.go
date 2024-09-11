d79c2b2f6a2a8a44b0734e856db2ecb5d601f73a
zSnails <erizojuan33@gmail.com>
2024-09-11 15:38:23 -0600

    feat: add some logs

--- cmd/streamx/main.go
+++ cmd/streamx/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 	api "github.com/zSnails/streamx/internal/api/handlers"
 	"github.com/zSnails/streamx/internal/api/middleware"
 	"github.com/zSnails/streamx/internal/db"
@@ -15,15 +18,21 @@ import (
 
 var log = logging.Get()
 
-func main() {
-	r := mux.NewRouter()
-	r.NotFoundHandler = middleware.Logger(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func getPostgresConnectionString() string {
+	return fmt.Sprintf("postgresql://%s:%s@localhost", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
+}
 
-	}))
+func main() {
+	godotenv.Load()
+	r := mux.NewRouter()
+	r.NotFoundHandler = middleware.Logger(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+
 	mux.CORSMethodMiddleware(r)
 	r.Use(middleware.Logger)
 	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, "postgresql://postgres@localhost")
+
+    log.Infof("connecting to postgres database using user %s\n", os.Getenv("DB_USER"))
+	conn, err := pgx.Connect(ctx, getPostgresConnectionString())
 	if err != nil {
 		log.Panic(err)
 	}
