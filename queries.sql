168bc92da84aa7e30f2a2390e62cfe9fd732b630
zSnails <erizojuan33@gmail.com>
2024-09-11 15:39:26 -0600

    refactor: use `similarity` instead of `ILIKE`

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- name: FindMedia :many
-SELECT * FROM media WHERE title ILIKE $1;
+SELECT * FROM media WHERE SIMILARITY(title, $1) > 0.1;
 
 -- name: AllMedia :many
 SELECT * FROM media;
@@ -7,4 +7,5 @@ SELECT * FROM media;
 -- name: CreateMedia :exec
 INSERT INTO media (hash, title, creator) VALUES ($1, $2, $3);
 
---SELECT * FROM media WHERE SIMILARITY(title, $1) > 0.3;
+-- name: MediaExists :one
+SELECT exists (SELECT 1 FROM media WHERE hash = $1 LIMIT 1);
