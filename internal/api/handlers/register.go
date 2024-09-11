7b5d15269ca6761b6f850a4c83f7507a0a90355c
zSnails <erizojuan33@gmail.com>
2024-09-11 15:38:33 -0600

    fix: add missing parameters for testing

--- internal/api/handlers/register.go
+++ internal/api/handlers/register.go
@@ -29,23 +29,50 @@ func upload(queries *db.Queries) http.HandlerFunc {
 			return
 		}
 
+		title := r.FormValue("title")
+		if title == "" {
+			log.Errorln("missing title from form")
+			http.Error(w, "missing title from form", http.StatusBadRequest)
+			return
+		}
+		creator := r.FormValue("creator")
+		if creator == "" {
+			log.Errorln("missing creator from form")
+			http.Error(w, "missing creator from form", http.StatusBadRequest)
+			return
+		}
+
 		file, header, err := r.FormFile("media")
 		if err != nil {
 			log.Errorln(err.Error())
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		log.Debugf("title: %v\n", title)
+		log.Debugf("creator: %v\n", creator)
+		log.Debugf("header.Filename: %v\n", header.Filename)
+		hash := uuid.NewSHA1(uuid.NameSpaceURL, []byte(header.Filename+creator+title))
+		log.Debugf("hash: %v\n", hash.String())
+		if err := hls.ConvertStream("hls", hash.String(), file); err != nil {
+			log.Error(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		log.Debugf("header.Filename: %v\n", header.Filename)
-        hash := uuid.NewSHA1(uuid.NameSpaceURL, []byte(header.Filename))
-		log.Debugf("hash: %v\n", hash.String())
-		hls.ConvertStream("hls", hash.String(), file)
 
-		// queries.CreateMedia(r.Context(), db.CreateMediaParams{
-		// 	Hash:    "",
-		// 	Title:   "",
-		// 	Creator: "",
-		// })
+		if err := queries.CreateMedia(r.Context(), db.CreateMediaParams{
+			Hash:    hash.String(),
+			Title:   title,
+			Creator: creator,
+		}); err != nil {
+			log.Errorln(err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	}
+}
+
+type searchResponse struct {
+	Count int         `json:"count"`
+	Media []db.Medium `json:"media"`
 }
 
 func search(queries *db.Queries) http.HandlerFunc {
