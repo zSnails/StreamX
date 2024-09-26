13f8d2c9c907f4238e9c5b741e90112dba148b50
zSnails <erizojuan33@gmail.com>
2024-09-25 20:04:43 -0600

    refactor: we now use a `pgxpool.Conn` instead of a `pgx.Conn`

--- cmd/streamx/main.go
+++ cmd/streamx/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	api "github.com/zSnails/streamx/internal/api/handlers"
 	"github.com/zSnails/streamx/internal/api/middleware"
@@ -16,14 +17,49 @@ import (
 	"github.com/zSnails/streamx/pkg/logging"
 )
 
-var log = logging.Get()
+var (
+	log          = logging.Get()
+	instancePort string
+	dbUser       string
+	dbPassword   string
+	dbHost       string
+)
 
 func getPostgresConnectionString() string {
-	return fmt.Sprintf("postgresql://%s:%s@localhost", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
+	return fmt.Sprintf("postgresql://%s:%s@%s", dbUser, dbPassword, dbHost)
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Warnln(err)
+	}
+
+	flag.StringVar(&instancePort, "port", os.Getenv("PORT"), "the port on which to launch this instance")
+	flag.StringVar(&dbUser, "postgres-user", os.Getenv("DB_USER"), "the user of the postgres database")
+	flag.StringVar(&dbPassword, "postgres-password", os.Getenv("DB_PASSWORD"), "the password of the postgres database")
+	flag.StringVar(&dbHost, "postgres-host", os.Getenv("DB_HOST"), "the host of the postgres database")
+	flag.Parse()
+
+	if instancePort == "" {
+		log.Panicln("the instance port can't be unset either set it on the .env file or pass it as an argument ")
+	}
+
+	if dbUser == "" {
+		log.Panicln("the user of the postgres database can't be unset either set it on the .env file or pass it as an argument ")
+	}
+
+	if dbPassword == "" {
+		log.Panicln("the password of the postgres database can't be unset either set it on the .env file or pass it as an argument ")
+	}
+
+	if dbHost == "" {
+		log.Panicln("the host of the postgres database can't be unset either set it on the .env file or pass it as an argument ")
+	}
+
 }
 
 func main() {
-	godotenv.Load()
 	r := mux.NewRouter()
 	r.NotFoundHandler = middleware.Logger(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
 
@@ -31,8 +67,9 @@ func main() {
 	r.Use(middleware.Logger)
 	ctx := context.Background()
 
-    log.Infof("connecting to postgres database using user %s\n", os.Getenv("DB_USER"))
-	conn, err := pgx.Connect(ctx, getPostgresConnectionString())
+	log.Infof("connecting to postgres database using user %s\n", dbUser)
+	conn, err := pgxpool.New(ctx, getPostgresConnectionString())
+	// conn, err := pgx.Connect(ctx, getPostgresConnectionString())
 	if err != nil {
 		log.Panic(err)
 	}
@@ -42,10 +79,10 @@ func main() {
 	})
 
 	queries := db.New(conn)
-	api.API(r, queries)
-	r.Handle("/{hash}/{file}", middleware.FilesMW(http.FileServer(http.Dir("hls"))))
+	api.API(r, queries, conn)
+	// r.Handle("/{hash}/{file}", middleware.FilesMW(http.FileServer(http.Dir("hls"))))
 
-	if err := http.ListenAndServe(":8080", handlers.CORS()(r)); err != nil {
+	if err := http.ListenAndServe(instancePort, handlers.CORS()(r)); err != nil {
 		log.Panic(err)
 	}
 }
